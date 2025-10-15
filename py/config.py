50f086979cfb91c17e95d81e5eeac188dfc7daf3
Laurens Valk <laurens@pybricks.com>
2025-10-15 19:44:19 +0200

    !squash: LUIGI config

--- py/config.py
+++ py/config.py
@@ -1,11 +1,24 @@
+# # Pybricks BLE Broadcast channel used between the hubs
+# CHANNEL = 45
+
+# # Mario Hub type: MARIO, LUIGI, or PEACH.
+# MARIO_TYPE = "MARIO"
+
+# # Bluetooth name of the remote. Choose None if any remote may be used.
+# REMOTE_NAME = MARIO_TYPE
+
+# # Whether to use pairing for connecting (needed if Mario was updated.)
+# PAIR = True
+
+
 # Pybricks BLE Broadcast channel used between the hubs
-CHANNEL = 45
+CHANNEL = 123
 
 # Mario Hub type: MARIO, LUIGI, or PEACH.
-MARIO_TYPE = "MARIO"
+MARIO_TYPE = "LUIGI"
 
 # Bluetooth name of the remote. Choose None if any remote may be used.
 REMOTE_NAME = MARIO_TYPE
 
 # Whether to use pairing for connecting (needed if Mario was updated.)
-PAIR = True
+PAIR = False
